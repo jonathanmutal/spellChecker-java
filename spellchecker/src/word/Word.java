1200e90c96193f778481ff458ed55216d86b7bb0
jonathanmutal95 <jonathanmutal95@gmail.com>
2015-05-27 21:24:32 -0300

    s

--- spellchecker/src/word/Word.java
+++ spellchecker/src/word/Word.java
@@ -1,28 +1,69 @@
 package word;
 
+/**
+ * Esta clase representa una palabra del diccionario.
+ * La clase Word representa una palabra y es un wrapper sobre el tipo de datos String.
+ */
+
 public class Word {
+
+	/**
+	 * Este atributo representa la palabra mediante un String.
+	 */
+
 	private String word;
+
+	/**
+	 * Primer constructor de la clase.
+	 */
 
 	public Word(){
 		word = new String("");
 	}
 
+	/**
+	 * Segundo constructor de la clase.
+	 * @param s string que definira el word.
+	 */
+	
 	public Word(String s){
 		word = new String(s);
 	}
+
+	/**
+	 * Setea un Word con un String.
+	 * @param s Un string para setear el word.
+	 * @return void
+	 */
 
 	public void setWord(String s){
 		word = s;
 	}
 
+	/**
+	 * Obtiene un String del Word.
+	 * @return El string de la palabra(Word).
+	 */
+
 	public String getWord() {
 		return word;
 	}
+
+	/**
+	 * Método sobre-escrito, asocia un objeto Word con un código hash.
+	 * @return El código hash asociado a la palabra.
+	 */
 
 	@Override
 	public int hashCode() {
 		return  word.hashCode();
 	}
+
+	/**
+	 * Método sobre-escrito, indica cuando algún objeto es igual a este.
+	 * @param obj La referencia de un objeto que se quiere comparar.
+	 * @return true si el objeto es el mismo que el argumento, false si no.
+	 */
 
 	@Override
 	public boolean equals(Object obj) {
--- spellchecker/src/word/WordSet.java
+++ spellchecker/src/word/WordSet.java
@@ -4,13 +4,34 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
+/**
+ * 
+ * La clase WordSet representa un conjunto de palabras, el cual se implementa interna-mente con una
+ * coleccion que implemente la interfaz Set
+ *
+ */
+
 public class WordSet {
+
+	/**
+	 * Representa un conjunto de palabras, un set.
+	 */
+	
 	private Set<Word> set;
+
+	/**
+	 * Construye un nuevo conjunto vacío
+	 */
 
 	public WordSet() {
 		this.set = new HashSet<Word>(); 
 	}
 
+	/**
+	 * Agrega el elemento word, si es que no se encuentra en el conjunto.
+	 * @param word elemento que sera agregado al conjunto.
+	 */
+	
 	public void add(Word word) {
 		this.set.add(word);
 	}
@@ -22,6 +43,11 @@ public class WordSet {
 	public void clear() {
 		this.set.clear();
 	}
+	
+	/**
+	 * Devuelve una referencia a un iterator correspondiente a la implementacion interna del conjunto.
+	 * @return una referencia a un iterator correspondiente a la implementacion interna del conjunto.
+	 */
 	
 	public Iterator<Word> iterator() {
 		Iterator<Word> iterador = set.iterator();
